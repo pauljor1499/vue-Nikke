e1d62b4e8f01985a51fc22aaf5df678fa68c32b3
Paul J <jorpaulvincent@gmail.com>
2022-04-22 12:19:25 +0800

    update responsive pages

--- src/components/Admin/AdminDashboard.vue
+++ src/components/Admin/AdminDashboard.vue
@@ -4,55 +4,60 @@
       <div class="container-head">
         <h1>Welcome to your Dashboard!</h1>
       </div>
-      <div class="cards-4">
-        <div class="card">
-          <n-card
-            title="Product List"
-            :segmented="{
-              content: true,
-              footer: 'soft',
-            }"
-            hoverable
-          >
-            <template #header-extra>View</template>
-            Card Content
-          </n-card>
-        </div>
-        <div class="card">
-          <n-card
-            title="User List"
-            :segmented="{
-              content: true,
-              footer: 'soft',
-            }"
-            hoverable
-          >
-            <template #header-extra>View</template>
-            Card Content
-          </n-card>
-        </div>
-        <div class="card">
-          <n-card
-            title="Notifications"
-            :segmented="{
-              content: true,
-              footer: 'soft',
-            }"
-            hoverable
-          >
-            <template #header-extra>View</template>
-            Card Content
-          </n-card>
-        </div>
-        <div class="card">
-          <n-card class="calendar" hoverable>
-            <div class="date">21</div>
-            <div>
-              <div class="month">December</div>
-              <div class="year">2018</div>
-              <div class="day">Sunday</div>
-            </div>
-          </n-card>
+      <div class="card-content">
+        <div class="cards">
+          <div class="card">
+            <n-card
+              title="Product List"
+              :segmented="{
+                content: true,
+                footer: 'soft',
+              }"
+              hoverable
+            >
+              <template #header-extra>View</template>
+              Card Content
+            </n-card>
+          </div>
+
+          <div class="card">
+            <n-card
+              title="User List"
+              :segmented="{
+                content: true,
+                footer: 'soft',
+              }"
+              hoverable
+            >
+              <template #header-extra>View</template>
+              Card Content
+            </n-card>
+          </div>
+
+          <div class="card">
+            <n-card
+              title="Notifications"
+              :segmented="{
+                content: true,
+                footer: 'soft',
+              }"
+              hoverable
+            >
+              <template #header-extra>View</template>
+              Card Content
+            </n-card>
+          </div>
+
+          <div class="card">
+            <n-card class="calendar" hoverable>
+              <div class="date">21</div>
+              <div>
+                <div class="month">December</div>
+                <div class="year">2018</div>
+                <div class="day">Sunday</div>
+              </div>
+            </n-card>
+          </div>
         </div>
       </div>
     </div>
@@ -81,37 +86,44 @@ export default {
 }
 
 .container .container-head {
-  padding: 50px 20px 120px 100px;
+  padding: 80px 20px 120px 0px;
   background-color: gray;
   overflow: hidden;
 }
 
 .container .container-head h1 {
   color: white;
+  margin-left: 5%;
   float: left;
 }
 
+.container .card-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.cards {
+  /* display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  column-gap: 20px;
+  padding: 20px; */
+
+  display: flex;
+  flex-wrap: wrap;
+  grid-gap: 10px;
+  margin-top: -50px;
+}
+
 .card {
-  width: 100%;
+  /* width: 100%;
   height: 200px;
   text-align: left;
   margin-top: -80px;
-  cursor: pointer;
-}
+  cursor: pointer; */
 
-.cards-4 {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  column-gap: 20px;
-  padding: 20px;
+  width: 250px;
 }
-
-/* .cards-5 {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  column-gap: 20px;
-  padding: 20px;
-} */
 
 .card .calendar {
   display: flex;
